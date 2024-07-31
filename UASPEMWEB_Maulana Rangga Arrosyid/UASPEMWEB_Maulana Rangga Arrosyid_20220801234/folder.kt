692da9f767d35bac504514cb52d53cacff24b38c
Maulana Rangga Arrosyid <134927221+rangga13092004@users.noreply.github.com>
2024-07-31 22:35:21 +0700

    Delete UASPEMWEB_Maulana Rangga Arrosyid directory

--- UASPEMWEB_Maulana Rangga Arrosyid/UASPEMWEB_Maulana Rangga Arrosyid_20220801234/folder.kt
+++
@@ -1,6 +0,0 @@
-project-folder/
-├── Dockerfile
-├── docker-compose.yml
-├── src/ (kode sumber aplikasi web Anda)
-└── db/
-    └── data/ (data basis data MySQL Anda)
--- UASPEMWEB_Maulana Rangga Arrosyid/UASPEMWEB_Maulana Rangga Arrosyid_20220801234/index.lua
+++
@@ -1,4 +0,0 @@
-project-folder/
-├── style.css
-├── login.html
-└── path/to/logo.png (sesuaikan dengan path gambar logo Anda)
